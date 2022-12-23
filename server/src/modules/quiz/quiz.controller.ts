fac0d4f3a4159d789de20c6f630073df34a1f93b
siyer696 <sachiniyermalkapur@gmail.com>
2022-12-23 18:44:21 +0530

    inject a service

--- server/src/modules/quiz/quiz.controller.ts
+++ server/src/modules/quiz/quiz.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { QuizService } from './quiz.service';
 
 @Controller('quiz')
 export class QuizController {
+  constructor(private readonly quizService: QuizService) {}
 
-    @Get('/')
-    getAllQuiz() {
-        return [1, 2, 3];
-    }
+  @Get('/')
+  getAllQuiz() {
+    return this.quizService.getAllQuiz();
+  }
 }
