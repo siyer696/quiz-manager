94761a10ccb6073ab7b921b18ae327a59f75ae91
siyer696 <sachiniyermalkapur@gmail.com>
2022-12-29 22:02:45 +0530

    added configurations to env file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { typeOrmConfig } from './config/typeorm.config';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 import { QuizModule } from './modules/quiz/quiz.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [QuizModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    QuizModule,
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
